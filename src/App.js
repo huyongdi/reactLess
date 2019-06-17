3806313e0f41dc699e044211d1ff182ff0d72006
HYD <354224080@qq.com>
2019-06-17 09:06:49 +0800

    '体检'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { Button} from 'antd';
+import style from './test.module.less'
+
 function App() {
+  console.log(style)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button type="primary">
+          Dashed
+        </Button>
+        <span className={style.abc}>123</span>
       </header>
     </div>
   );
